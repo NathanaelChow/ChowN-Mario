b0306a33adbd3a05ae37a256d209abe4c77ed552
NathanaelChow <nhchowder@gmail.com>
2014-11-21 09:20:29 -0800

    Video 12

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -18,7 +18,7 @@ game.PlayerEntity = me.Entity.extend({
         this.renderable.setCurrentAnimation("idle");
 
         this.body.setVelocity(5, 20);
-
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
     update: function(delta) {
         
@@ -39,6 +39,7 @@ game.PlayerEntity = me.Entity.extend({
         } else {
             this.renderable.setCurrentAnimation("idle");
         }
+        
 
         this._super(me.Entity, "update", [delta]);
         return true;
@@ -60,6 +61,7 @@ game.LevelTrigger = me.Entity.extend({
     onCollision: function(){
         this.body.setCollisionMask(me.collision.types.NO_OBJECT);
         me.levelDirector.loadLevel(this.level);
+        me.state.current().resetPlayer();
     }
     
     });
